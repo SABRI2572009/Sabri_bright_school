e17b4a17dd1c64ce2740460cfe03388d8300f7ab
SABRI2572009 <ishimwengambasabri@gmail.com>
2025-05-05 11:55:28 +0200

    Project
    New changes

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.parterns-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 10,
+    nav: true,
+    navText: [
+        "<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>"
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+});
